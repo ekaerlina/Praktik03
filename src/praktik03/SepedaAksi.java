883cd9ecbcbeb2ff9cbef76f5c28c4c30ebba679
ekaerlina <eka.erlinaa11@gmail.com>
2022-12-16 18:18:50 +0800

    Menggunakan Method Get

--- src/praktik03/SepedaAksi.java
+++ src/praktik03/SepedaAksi.java
@@ -9,6 +9,11 @@ public class SepedaAksi {
         
         s.cetakInfo();
         
-     
+        System.out.print("Merknya \t: ");
+        System.out.println(s.getMerk());
+        System.out.print("Warnanya \t: ");
+        System.out.println(s.getWarna());
+        System.out.print("Harganya \t: ");
+        System.out.println(s.getHarga());
     }
 }
