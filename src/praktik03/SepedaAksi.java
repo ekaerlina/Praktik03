7ae65d40169418b14ecba7073a1e34eb88ed6bcf
ekaerlina <eka.erlinaa11@gmail.com>
2022-12-16 17:25:30 +0800

    Buat class Aksi

--- src/praktik03/SepedaAksi.java
+++ src/praktik03/SepedaAksi.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package praktik03;
-
-/**
- *
- * @author Bayu Kusuma
- */
 public class SepedaAksi {
-    
+    public static void main(String[] args) {
+        
+    }
 }
