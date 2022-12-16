3c08114989aab3ff7d336f3d59adc11e9f0857ab
ekaerlina <eka.erlinaa11@gmail.com>
2022-12-16 17:52:55 +0800

    buat Getter dan Setter

--- src/praktik03/SepedaAksi.java
+++ src/praktik03/SepedaAksi.java
@@ -2,6 +2,7 @@ package praktik03;
 public class SepedaAksi {
     public static void main(String[] args) {
         Sepeda s = new Sepeda();
+
         s.merk="Kupu-kupu";
         s.warna="Merah";
         s.harga=1000;
