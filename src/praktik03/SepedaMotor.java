acafd9fdaa59c9794331beb3a18df9b38e260be1
ekaerlina <eka.erlinaa11@gmail.com>
2022-12-16 18:49:23 +0800

    Attribute dan Method

--- src/praktik03/SepedaMotor.java
+++ src/praktik03/SepedaMotor.java
@@ -1,5 +1,9 @@
 package praktik03;
 
 public class SepedaMotor extends Sepeda{
+    private String jenisMotor;
     
+    void throttle(){
+        System.out.println("NGEEEENG......");
+            }
 }
